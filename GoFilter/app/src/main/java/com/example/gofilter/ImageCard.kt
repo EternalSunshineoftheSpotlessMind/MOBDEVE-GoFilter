f78e39390fc726ad0b27e3843e504361e262cff6
John Sarmiento <johnsarmiento117@gmail.com>
2023-07-30 15:28:47 +0800

    Initial frontend design with Bottom Navigation Bar and Home Feed Screen

---
+++ GoFilter/app/src/main/java/com/example/gofilter/ImageCard.kt
@@ -0,0 +1,59 @@
+package com.example.gofilter
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
+import kotlin.random.Random
+
+@Composable
+fun ImageCard(
+    title: String,
+    description: String,
+    modifier: Modifier
+) {
+    Card(
+        modifier = Modifier.padding(16.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surfaceVariant
+        ),
+        shape = MaterialTheme.shapes.large
+    ) {
+        Image(
+            painter = rememberAsyncImagePainter(
+                model = "https://picsum.photos/seed/${Random.nextInt()}/300/200"
+            ),
+            contentDescription = null,
+            modifier = Modifier
+                .clip(MaterialTheme.shapes.large)
+                .fillMaxWidth()
+                .aspectRatio(3f / 2f)
+        )
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+        ) {
+            Text(
+                text = title,
+                style = MaterialTheme.typography.titleLarge
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = description,
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }
+    }
+}
